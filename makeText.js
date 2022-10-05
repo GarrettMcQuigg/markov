f2495178de99c788cd7fd550e1831194fb6a7d57
Garrett McQuigg <garrettmcquigg@gmail.com>
2022-10-05 16:29:15 -0500

    1

--- makeText.js
+++ makeText.js
@@ -1,2 +1,43 @@
-/** Command-line tool to generate Markov text. */
+const markov = require("./markov");
+const fs = require("fs");
+const process = require("process");
+const axios = require("axios");
 
+function ranText(text) {
+  let markMach = new markov.MarkovMachine(text);
+  console.log(markMach.makeText());
+}
+
+function text(path) {
+  fs.readFile(path, "utf8", function cb(err, data) {
+    if (err) {
+      console.error(`${err}`);
+      process.exit(1);
+    } else {
+      ranText(data);
+    }
+  });
+}
+
+async function makeURLText(url) {
+  let res;
+
+  try {
+    res = await axios.get(url);
+  } catch (err) {
+    console.error(`${err}`);
+    process.exit(1);
+  }
+  ranText(res.data);
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+  text(path);
+} else if (method === "url") {
+  makeURLText(path);
+} else {
+  console.error(`Unknown method: ${method}`);
+  process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -1,13 +1,9 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
-  /** build markov machine; read in text.*/
-
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +13,39 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWord]);
+    }
+
+    this.chains = chains;
   }
 
-
-  /** return random text from chains */
+  static ranChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
   makeText(numWords = 100) {
-    // TODO
+    
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+
+    return output.join(" ");
   }
 }
+
+
+module.exports = {
+  MarkovMachine,
+};
